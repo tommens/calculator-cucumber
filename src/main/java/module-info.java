41fde436160b42493a0febdb8faf5c615693d30d
FlorianiLaurence <laurenceflo@hotmail.com>
2022-03-22 16:14:48 +0100

    fix module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module gui.calculator {
     requires javafx.controls;
     requires javafx.fxml;
     requires org.kordamp.bootstrapfx.core;
+    requires org.antlr.antlr4.runtime;
 
     opens gui to javafx.fxml;
     exports gui;
