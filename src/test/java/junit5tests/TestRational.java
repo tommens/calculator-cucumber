a38c04e6dc926a1f0d31c8252d10bfeaca66adc4
Thomas Lavend'Homme <lavendthomas@outlook.be>
2022-03-22 09:11:50 +0100

    Add JUnit tests for Rational

--- src/test/java/junit5tests/TestRational.java
+++ src/test/java/junit5tests/TestRational.java
@@ -11,19 +11,28 @@ import java.util.ArrayList;
 
 public class TestRational {
 
-	private final int value =8;
+	private final int value = 8;
+	private final int num1  = 1;
+	private final int den1  = 4;
+	private final int num2  = 2;
+	private final int den2  = 5;
 	private Number number;
-	
+	private Rational a;
+	private Rational b;
+
+
 	@BeforeEach
 	public void setUp() {
 		number = new Rational(value);
+		a = new Rational(num1, den1);
+		b = new Rational(num2, den2);
 	}
 
 	@Test
 	public void testEquals() {
-		// Two distinct MyNumber, constructed separately (using a different constructor) but containing the same value should be equal
+		// Two distinct Rationals, constructed separately (using a different constructor) but containing the same value should be equal
 		assertEquals(new Rational(value), number);
-		// Two MyNumbers containing a distinct value should not be equal:
+		// Two Rationals containing a distinct value should not be equal:
 		int otherValue = 7;
 		assertNotEquals(new Rational(otherValue),number);
 		assertEquals(number, number); // Identity check (for coverage, as this should always be true)
@@ -32,6 +41,9 @@ public class TestRational {
 			assertNotEquals(new Times(new ArrayList<>()), number);
 		}
 		catch (IllegalConstruction e) {fail();}
+		// Two Rationals created with different simplification factors should be equal:
+		Rational a_alt = new Rational(2, 8);
+		assertEquals(a, a_alt);
 	}
 
 	@Test
@@ -56,5 +68,50 @@ public class TestRational {
 	public void testToString() {
 		assertEquals(Integer.toString(value), number.toString());
 	}
+
+	@Test
+	public void testAddition() {
+		assertEquals(new Rational(13, 20), a.add(b));
+	}
+
+	@Test
+	public void testAdditionSymmetry() {
+		assertEquals(b.add(a), a.add(b));
+	}
+
+	@Test
+	public void testDifference() {
+		assertEquals(new Rational(-3, 20), a.subtract(b));
+	}
+
+	@Test
+	public void testMultiplication() {
+		assertEquals(new Rational(2, 20), a.multiply(b));
+	}
+
+	@Test
+	public void testMultiplicationSymmetry() {
+		assertEquals(b.multiply(a), a.multiply(b));
+	}
+
+	@Test
+	public void testDivision() {
+		assertEquals(new Rational(5, 8), a.divide(b));
+	}
+
+	@Test
+	public void testNegation() {
+		assertEquals(new Rational(-1, 4), a.negate());
+	}
+
+	@Test
+	public void testComparison() {
+		assertTrue(a.compareTo(b) > 0);
+		assertTrue(b.compareTo(a) < 0);
+		assertEquals(0, a.compareTo(new Rational(num1, den1)));
+	}
+
+
+
 
 }
