e2a29a910d2aa4809ab2f864165b7ddde8c99c0e
momosahi <mohamedfrancissahi@gmail.com>
2022-04-11 05:51:06 +0200

    Added test for toString() method.

--- src/test/java/junit5tests/TestMyBoolean.java
+++ src/test/java/junit5tests/TestMyBoolean.java
@@ -30,4 +30,27 @@ public class TestMyBoolean {
         assertNotEquals(new MyBoolean(otherValue),myBoolean);
         assertEquals(myBoolean, myBoolean); // Identity check (for coverage, as this should always be true)
     }
+
+    @Test
+    public void testCountDepth() {
+        //test whether a number has zero depth (i.e. no nested expressions)
+        assertEquals(Integer.valueOf(0), myBoolean.countDepth());
+    }
+
+    @Test
+    public void testCountOps() {
+        //test whether a number contains zero operations
+        assertEquals(Integer.valueOf(0), myBoolean.countOps());
+    }
+
+    @Test
+    public void testCountNbs() {
+        //test whether a number contains 1 number
+        assertEquals(Integer.valueOf(1), myBoolean.countNbs());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(value?"1":"0", myBoolean.toString());
+    }
 }
