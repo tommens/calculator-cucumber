6ee6036753512c0de81c7ce6996085fa38011636
Randi <dev@dochot.be>
2023-03-08 19:17:45 +0100

    Add : add main to cli

---
+++ src/main/java/cli/Main.java
@@ -0,0 +1,42 @@
+package cli;
+
+import java.util.Scanner;
+
+public class Main
+{
+    protected static boolean isRunning = true;
+    public static void helloWord()
+    {
+        System.out.print("Calculator Cucumber\n This is a calculator that can be used to perform basic arithmetic operations.\n");
+    }
+
+    public static void printMenu()
+    {
+        System.out.println("Please enter an expression to evaluate: ");
+    }
+
+    public static void get_input()
+    {
+        System.out.print("$> ");
+        Scanner scanner = new Scanner(System.in);
+        String inputUser = scanner.nextLine();
+        if (inputUser.equals(".quit"))
+        {
+            isRunning = false;
+        }
+        else
+        {
+            System.out.println("You entered: " + inputUser);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        helloWord();
+        printMenu();
+        while(isRunning)
+        {
+            get_input();
+        }
+    }
+}
