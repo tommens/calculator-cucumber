f9ae42d0ca2553783cc629b147f7912eb2f9edb6
Randi <dev@dochot.be>
2023-03-16 23:06:05 +0100

    Add : arithmetic modulo

---
+++ src/main/java/calculator/arithmetics/Modulo.java
@@ -0,0 +1,52 @@
+package calculator.arithmetics;
+
+import calculator.Expression;
+import calculator.IllegalConstruction;
+import calculator.Notation;
+import calculator.Operation;
+
+import java.util.List;
+
+public class Modulo extends Operation
+{
+
+    /**
+     * It is not allowed to construct an operation with a null list of expressions.
+     * Note that it is allowed to have an EMPTY list of arguments.
+     *
+     * @param elist The list of expressions passed as argument to the arithmetic operation
+     * @throws IllegalConstruction Exception thrown if a null list of expressions is passed as argument
+     */
+    protected Modulo(List<Expression> elist) throws IllegalConstruction
+    {
+        this(elist, null);
+    }
+
+
+    /**
+     * It is not allowed to construct an operation with a null list of expressions.
+     * Note that it is allowed to have an EMPTY list of arguments.
+     *
+     * @param elist The list of expressions passed as argument to the arithmetic operation
+     * @throws IllegalConstruction Exception thrown if a null list of expressions is passed as argument
+     */
+    protected Modulo(List<Expression> elist, Notation notation) throws IllegalConstruction
+    {
+        super(elist, notation);
+        symbol = "%";
+        neutral = 0;
+    }
+
+    /**
+     * Abstract method representing the actual binary arithmetic operation to compute
+     *
+     * @param l first argument of the binary operation
+     * @param r second argument of the binary operation
+     * @return result of computing the binary operation
+     */
+    @Override
+    public int op(int l, int r)
+    {
+        return l % r;
+    }
+}
