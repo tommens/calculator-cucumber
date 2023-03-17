550d4bbb8621f8c1ed176d93e3787190331dedfe
Randi <dev@dochot.be>
2023-03-17 11:07:09 +0100

    Add : Combinatorial class

---
+++ src/main/java/calculator/arithmetics/Combinatorial.java
@@ -0,0 +1,67 @@
+package calculator.arithmetics;
+
+import calculator.Expression;
+import calculator.IllegalConstruction;
+import calculator.Notation;
+import calculator.Operation;
+
+import java.util.List;
+
+public class Combinatorial extends Operation
+{
+
+
+    /**
+     * It is not allowed to construct an operation with a null list of expressions.
+     * Note that it is allowed to have an EMPTY list of arguments.
+     *
+     * @param elist The list of expressions passed as argument to the arithmetic operation
+     * @throws IllegalConstruction Exception thrown if a null list of expressions is passed as argument
+     */
+    public Combinatorial(List<Expression> elist) throws IllegalConstruction
+    {
+        this(elist, null);
+    }
+
+
+    /**
+     * It is not allowed to construct an operation with a null list of expressions.
+     * Note that it is allowed to have an EMPTY list of arguments.
+     *
+     * @param elist The list of expressions passed as argument to the arithmetic operation
+     * @throws IllegalConstruction Exception thrown if a null list of expressions is passed as argument
+     */
+    public Combinatorial(List<Expression> elist, Notation notation) throws IllegalConstruction
+    {
+        super(elist, notation);
+        symbol = "comb";
+        neutral = 0;
+    }
+
+
+    private int comb(int a, int b)
+    {
+        int numerator = 1;
+        int denominator = 1;
+        for (int i = 0; i < b; i++)
+        {
+            numerator *= (a-i);
+            denominator *= (i+1);
+        }
+        return numerator / denominator;
+    }
+
+
+    /**
+     * Abstract method representing the actual binary arithmetic operation to compute
+     *
+     * @param l first argument of the binary operation
+     * @param r second argument of the binary operation
+     * @return result of computing the binary operation
+     */
+    @Override
+    public int op(int l, int r)
+    {
+        return 0;
+    }
+}
