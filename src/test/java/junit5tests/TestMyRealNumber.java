b4cfcb632226c9e60aa160dfc578d2cfa2a17b05
thoma <thomas.brenart@gmail.com>
2023-03-17 17:30:09 +0100

    Test for class MyRealNumber.java

---
+++ src/test/java/junit5tests/TestMyRealNumber.java
@@ -0,0 +1,40 @@
+package junit5tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+import calculator.*;
+
+import java.util.ArrayList;
+
+class TestMyRealNumber {
+
+    private final Double value =8.5;
+    private MyRealNumber number;
+
+    @BeforeEach
+    void setUp() {
+        number = new MyRealNumber(value);
+    }
+
+    @Test
+    void testEquals() {
+        // Two distinct MyNumber, constructed separately (using a different constructor) but containing the same value should be equal
+        assertEquals(new MyRealNumber(value), number);
+        // Two MyNumbers containing a distinct value should not be equal:
+        Double otherValue = 7.7;
+        assertNotEquals(new MyRealNumber(otherValue),number);
+        assertEquals(number, number); // Identity check (for coverage, as this should always be true)
+        assertNotEquals(number, value); // number is of type MyNumber, while value is of type int, so not equal
+        try {
+            assertNotEquals(new Times(new ArrayList<>()), number);
+        }
+        catch (IllegalConstruction e) {fail();}
+    }
+
+    @Test
+    void testToString() {
+        assertEquals(Double.toString(value), number.toString());
+    }
+
+}
