91d555533028031f16481c8a5e62b96be64efd1b
Guillaume Kerckhofs <guillaume.kerckhofs@student.umons.ac.be>
2023-03-18 16:22:59 +0100

    fix: adjust Evaluator to use complex number

--- src/main/java/visitor/Evaluator.java
+++ src/main/java/visitor/Evaluator.java
@@ -12,20 +12,20 @@ import java.util.ArrayList;
 public class Evaluator extends Visitor {
 
     /** The result of the evaluation will be stored in this private variable */
-    private int computedValue;
+    private MyNumber computedValue;
 
     /** getter method to obtain the result of the evaluation
      *
      * @return an Integer object containing the result of the evaluation
      */
-    public Integer getResult() { return computedValue; }
+    public MyNumber getResult() { return computedValue; }
 
     /** Use the visitor design pattern to visit a number.
      *
      * @param n The number being visited
      */
     public void visit(MyNumber n) {
-        computedValue = n.getValue();
+        computedValue = n;
     }
 
     /** Use the visitor design pattern to visit an operation
@@ -33,14 +33,14 @@ public class Evaluator extends Visitor {
      * @param o The operation being visited
      */
     public void visit(Operation o) {
-        ArrayList<Integer> evaluatedArgs = new ArrayList<>();
+        ArrayList<MyNumber> evaluatedArgs = new ArrayList<>();
         //first loop to recursively evaluate each subexpression
         for(Expression a:o.args) {
             a.accept(this);
             evaluatedArgs.add(computedValue);
         }
         //second loop to accumulate all the evaluated subresults
-        int temp = evaluatedArgs.get(0);
+        MyNumber temp = evaluatedArgs.get(0);
         int max = evaluatedArgs.size();
         for(int counter=1; counter<max; counter++) {
             temp = o.op(temp,evaluatedArgs.get(counter));
