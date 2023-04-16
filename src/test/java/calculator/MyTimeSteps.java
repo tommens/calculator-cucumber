105894cb12409da91c4d96af6d4263df1f71d734
YannMarchelli <yann.marchelli@etu.univ-cotedazur.fr>
2023-04-16 17:49:53 +0200

    Last code quality modifications

--- src/test/java/calculator/MyTimeSteps.java
+++ src/test/java/calculator/MyTimeSteps.java
@@ -13,21 +13,17 @@ import java.util.Collections;
 public class MyTimeSteps {
 
     private ArrayList<Expression> params;
-    private Operation op;
     private Calculator c;
     private Expression e;
-    private MyTime myTime;
-    private int number;
 
     @Before
     public void resetMemoryBeforeEachScenario() {
         params = null;
-        op = null;
     }
     @Given("A MyTime object with date {string}")
     public void GivenAMyTimeObjectWithDate(String date) {
         c = new Calculator();
-        this.myTime = new MyTime(date);
+        MyTime myTime = new MyTime(date);
         params = new ArrayList<>();
         long timeInSeconds = myTime.toSeconds();
         Collections.addAll(params,new MyNumber((int)timeInSeconds));
@@ -36,7 +32,6 @@ public class MyTimeSteps {
     @When("I provide a number to subtract {int}")
     public void whenIProvideANumberToSubtract(int val) throws IllegalConstruction {
         // Subtract the number from MyTime object
-        this.number = val;
         Collections.addAll(params,new MyNumber(val));
         e = new Minus(params, Notation.INFIX);
     }
@@ -44,7 +39,6 @@ public class MyTimeSteps {
     @When("I provide a number to add {int}")
     public void whenIProvideANumberToAdd(int val) throws IllegalConstruction {
         // Subtract the number from MyTime object
-        this.number = val;
         Collections.addAll(params,new MyNumber(val));
         e = new Plus(params, Notation.INFIX);
     }
