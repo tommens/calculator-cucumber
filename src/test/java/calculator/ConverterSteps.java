a37ac6645653dd2cc01bbaf995b220c7b2fb8e0b
YannMarchelli <yann.marchelli@etu.univ-cotedazur.fr>
2023-04-21 13:12:43 +0200

    Fixed issue
    Converted result in ConverterSteps from double to BigDecimal for the assertEquals

--- src/test/java/calculator/ConverterSteps.java
+++ src/test/java/calculator/ConverterSteps.java
@@ -6,17 +6,13 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class ConverterSteps {
-    private Calculator c;
     private Measurement measurement;
-    private String firstUnit;
-    private String secondUnit;
-    private double value;
+
     @Before
     public void resetMemoryBeforeEachScenario() {
         measurement = null;
@@ -24,45 +20,27 @@ public class ConverterSteps {
 
     @Given("a measurement {string} and a first unit {string}")
     public void givenAMeasurementAndAValue(String m, String unit1) {
-        String measure=m;
-        firstUnit=unit1;
-        switch (measure) {
-            case "Area" -> {
-                measurement = new Area(unit1);
-            }
-            case "Currency" -> {
-                measurement = new Currency(unit1);
-            }
-            case "Energy" -> {
-                measurement = new Energy(unit1);
-            }
-            case "Length" -> {
-                measurement = new Length(unit1);
-            }
-            case "Power" -> {
-                measurement = new Power(unit1);
-            }
-            case "Pressure" -> {
-                measurement = new Pressure(unit1);
-            }
-            case "Time" -> {
-                measurement = new Time(unit1);
-            }
+        switch (m) {
+            case "Area" -> measurement = new Area(unit1);
+            case "Currency" -> measurement = new Currency(unit1);
+            case "Energy" -> measurement = new Energy(unit1);
+            case "Length" -> measurement = new Length(unit1);
+            case "Power" -> measurement = new Power(unit1);
+            case "Pressure" -> measurement = new Pressure(unit1);
+            case "Time" -> measurement = new Time(unit1);
         }
     }
 
     @When("I provide a value {double}")
-    public void IProvideAValue(double val) {
-        value=val;
+    public void IProvideAValue() {
     }
 
     @When("a second unit {string}")
-    public void ASecondUnit(String unit2) {
-        secondUnit=unit2;
+    public void ASecondUnit() {
     }
 
     @Then("the conversion from {string} to {string} evaluates to {double}")
     public void theConversionEvaluatesTo(String unit1, String unit2, double result) {
-        assertEquals(result,measurement.printConversion(result,unit1,unit2));
+        assertEquals(BigDecimal.valueOf(result), Measurement.printConversion(result,unit1,unit2));
     }
 }
